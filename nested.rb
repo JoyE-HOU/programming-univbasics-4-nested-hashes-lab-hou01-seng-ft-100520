cfbb3f38f1ab8ab2f4facc5ddd68d2aea4a7f5ff
Joy Evans <joyevans@me.com>
2020-09-21 23:48:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,14 +21,14 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	programmer_hash = 
+	programmer_hash = "Object Orientation"
  		{
      # :grace_hopper => {
       #  :known_for => "COBOL",
        # :languages => ["COBOL", "FORTRAN"]
      # },
      # :alan_kay => {
-        :known_for => "Object Orientation",
+     #   :known_for => "Object Orientation",
     #    :languages => ["Smalltalk", "LISP"]
       #},
     #  :dennis_ritchie => {
