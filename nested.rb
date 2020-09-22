99410f55a06c642f680e8a7c3d38f49c74b2fe9d
Joy Evans <joyevans@me.com>
2020-09-22 00:01:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,14 +90,14 @@ def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
 
-	programmer_hash = "GUI"
+	programmer_hash = 
  		{
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
       },
       :alan_kay => {
-        :known_for => "Object Orientation",
+        :known_for => "GUI",
         :languages => ["Smalltalk", "LISP"]
       },
       :dennis_ritchie => {
